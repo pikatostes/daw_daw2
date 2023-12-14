a967dbb5ea251948a5615aa299ac1de30b8d4a92
Alejandro Rios <alejandro.rios.cyt@gmail.com>
2023-12-14 02:29:17 +0100

    apuntes

---
+++ APUNTES-EXAMEN/DB-Adminer/sql/sample-00.sql
@@ -0,0 +1,9 @@
+CREATE TABLE student (
+    id int,
+    name VARCHAR(255)
+);
+
+INSERT INTO student(id, name) VALUES
+(1,'Ana'),
+(2,'Braulio'),
+(3,'Carmen');
---
+++ P02-Prácticas/ServidoresWeb/3-PHP/ejer3-php/index.php
@@ -0,0 +1,3 @@
+<?php
+    echo "HOLA BUENAS TARDES";
+?>
